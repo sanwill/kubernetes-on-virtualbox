3571f9a805a4b1a1314e85bfe5443751124875f7
sanwill <45527038+sanwill@users.noreply.github.com>
2022-07-25 21:48:32 -0500

    Update k8s_install_ubuntu_allnodes.sh
    Updated, to use containerd as CRI runtime.

--- scripts/k8s_install_ubuntu_allnodes.sh
+++ scripts/k8s_install_ubuntu_allnodes.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+#Updated 24-07-2022
+
+#!/bin/bash
+#Updated 24-07-2022
 
 echo -e "\n###############" 
 echo      "Set OS - Ubuntu"
 echo      "###############"
 
-set -x
 sudo swapoff -a
 sudo sed -i '/\/swap.img/s/^/#/' /etc/fstab
 
@@ -12,54 +15,53 @@ sudo modprobe br_netfilter
 lsmod | grep br_netfilter
 
 
+# Lab only
+#systemctl disable firewalld --now 
+
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
 br_netfilter
 EOF
 
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 EOF
 sudo sysctl --system
 
 
 echo -e "\n##############" 
-echo -e   "Install Docker"
+echo -e   "Install containerd"
 echo      "##############"
 
 #https://docs.docker.com/engine/install/ubuntu/
-sudo apt-get update -y 
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update -y
 
-sudo apt-get install  ca-certificates curl gnupg lsb-release -y
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -p /etc/apt/keyrings
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-sudo docker run hello-world -y
+sudo docker run hello-world
 
-#https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
-sudo mkdir /etc/docker
-cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-
-sudo systemctl enable docker
-sudo systemctl daemon-reload
-sudo systemctl restart docker
+#https://kubernetes.io/docs/setup/production-environment/container-runtimes/#containerd
+#Make sure that cri is not included in thedisabled_plugins list
+sed -i 's/cri//g' /etc/containerd/config.toml
+sudo systemctl restart containerd
 
 
 echo -e "\n#########################################" 
@@ -80,3 +82,18 @@ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https:/
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+
+
+#On Master node
+# kubeadm init --pod-network-cidr 10.246.0.0/16 --apiserver-advertise-address=192.168.61.3
+#  mkdir -p $HOME/.kube;   sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config;   sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#Install Weave network CNI
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+#apt install etcd-client -y
+
+
+#On worker node
+# kubeadm join 192.168.61.3:6443 --token i8i36l.v4am9xluq8mwe4sl --discovery-token-ca-cert-hash sha256:6b9e4027a53be134ca013221c7407e784732d7f9de1fdc03a851bc3971aa6e57 
